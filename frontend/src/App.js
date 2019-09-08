0acf64d669eaa9f6e67cf8400ffc7923fdc2c86a
diogofalken <diogofalken0@gmail.com>
2019-09-08 11:36:03 +0100

    Login page structure done, added assets folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
 
+import Login from './pages/Login';
 function App() {
-  return <h1>Hello Rocketseat</h1>;
+  return <Login />;
 }
 
 export default App;
